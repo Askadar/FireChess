8c7b73865709ff818584e5bc6cb18047ecea6f8f
ZN <askadar.ru@gmail.com>
2022-04-11 17:30:24 +0300

    migrated App from svelte/rollup, to vue/parcel

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,16 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
+
 const firebaseConfig = {
-    apiKey: "AIzaSyC6nb2_r-JTRSVs5-9celW69fKCn4fcjUw",
-    authDomain: "firechess0.firebaseapp.com",
-    databaseURL: "https://firechess0-default-rtdb.firebaseio.com",
-    projectId: "firechess0",
-    storageBucket: "firechess0.appspot.com",
-    messagingSenderId: "897411439363",
-    appId: "1:897411439363:web:bca1b67cd29e78e71e6736",
-    measurementId: "G-F7N1H13P3R"
+    apiKey: process.env.FB_API_KEY,
+    authDomain: process.env.FB_AUTH_DOMAIN,
+    databaseURL: process.env.FB_DB_URL,
+    projectId: process.env.FB_PROJECT_ID,
+    storageBucket: process.env.FB_BUCKET,
+    messagingSenderId: process.env.ANALYTICS_MESSAGING_ID,
+    appId: process.env.ANALYTICS_ID,
+    measurementId: process.env.ANALYTICS_MEASUREMENT_ID,
 };
 
 firebase.initializeApp(firebaseConfig);

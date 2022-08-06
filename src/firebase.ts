5ef2b38fb7bf899c778c1d8851dfb2424b58e84a
ZN <askadar.ru@gmail.com>
2022-08-06 17:11:08 +0300

    added real rules removed linked .firebaserc removed unnecessary env\configs

--- src/firebase.ts
+++ src/firebase.ts
@@ -5,9 +5,7 @@ import { getFirestore } from 'firebase/firestore'
 const firebaseConfig = {
 	apiKey: process.env.FB_API_KEY,
 	authDomain: process.env.FB_AUTH_DOMAIN,
-	databaseURL: process.env.FB_DB_URL,
 	projectId: process.env.FB_PROJECT_ID,
-	storageBucket: process.env.FB_BUCKET,
 	messagingSenderId: process.env.ANALYTICS_MESSAGING_ID,
 	appId: process.env.ANALYTICS_ID,
 	measurementId: process.env.ANALYTICS_MEASUREMENT_ID,
